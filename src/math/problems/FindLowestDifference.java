3fed2c286ef79f97b59ef90111b537b283a0b819
njahan98 <51012116+njahan98@users.noreply.github.com>
2020-11-09 01:11:54 -0500

    Update FindLowestDifference.java

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,29 +1,30 @@
 package math.problems;
 
 public class FindLowestDifference {
-
-    public static void main(String[] args) {
-        /*
+	 /*
          Implement in java.
          Read this below two array. Find the lowest difference between the two array cell.
          The lowest difference between cells is 1
         */
+    
+	public static void main(String[] args) {
+
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
-        System.out.println("The lowest difference between the two arrays is: " + lowDiff());
-        
+
+        System.out.println(lowDiff(array2, array1));
+
     }
-    static int lowDiff(int [] array, int s) {
-    	int difference = Integer.MAX_VALUE;
-		difference = Math.abs((array[i] - array[j])); 
-    	
-    	for (int i=0; i< s-1; i++) {
-    		for (int j=0; j<s; j++) {
-    			if(Math.abs((array[i] - array[j]) < difference){
-    				return difference; 
-    			}
-    		
-    	}
-    } 
+    static int lowDiff(int[] arr, int s)
+    {
+        int difference = Integer.MAX_VALUE;
+
+        for (int i=0; i<s-1; i++)
+            for (int j=i+1; j<s; j++)
+                if (Math.abs((arr[i] - arr[j]) )< difference)
+                    difference = Math.abs((arr[i] - arr[j]));
+
+        return difference;
+    }
 
 }
