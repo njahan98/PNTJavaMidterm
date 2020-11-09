576f8120a7f5304ec290ece529a664a75fa1f402
njahan98 <51012116+njahan98@users.noreply.github.com>
2020-11-09 00:58:39 -0500

    Update FindLowestDifference.java

--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -10,7 +10,20 @@ public class FindLowestDifference {
         */
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19};
-
+        System.out.println("The lowest difference between the two arrays is: " + lowDiff());
+        
     }
+    static int lowDiff(int [] array, int s) {
+    	int difference = Integer.MAX_VALUE;
+		difference = Math.abs((array[i] - array[j])); 
+    	
+    	for (int i=0; i< s-1; i++) {
+    		for (int j=0; j<s; j++) {
+    			if(Math.abs((array[i] - array[j]) < difference){
+    				return difference; 
+    			}
+    		
+    	}
+    } 
 
 }
