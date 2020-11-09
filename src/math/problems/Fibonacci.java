e13b5e5479b4963136306deb93893a5231dae5d3
njahan98 <51012116+njahan98@users.noreply.github.com>
2020-11-09 17:28:32 -0500

    Update Fibonacci.java

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -3,10 +3,23 @@ package math.problems;
 public class Fibonacci {
 
     public static void main(String[] args) {
+    	
+    	int index = 0;
+    	while (true) {
+    		System.out.println(fibonacci(index)); //fibonacci sequence done with recursion 
+    		index++; 
+    	}
          /*
           Write 40 Fibonacci numbers with java.
           0,1,1,2,3,5,8,13
          */
-
+    }
+    public static long fibonacci(int i) {
+    	
+    	if (i == 0) return 0;
+    	if (i <= 2) return 1;
+    	
+    	long fib = fibonacci(i-1) + fibonacci(i-2);
+    	return fib; 
     }
 }
