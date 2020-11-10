f3d00927329a791371f8911af90e5d57b2521319
njahan98 <51012116+njahan98@users.noreply.github.com>
2020-11-10 18:09:51 -0500

    Update UseMap.java

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,16 +1,34 @@
 package datastructure;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class UseMap {
 
     public static void main(String[] args) {
+    	Map<Integer, String> bangladesh = new LinkedHashMap<Integer, String>();
+
+        bangladesh.put(0, "Dhaka");
+        bangladesh.put(1, "Sylhet");
+        bangladesh.put(2, "Chandpur");
+        bangladesh.put(3, "Comila");
+        bangladesh.put(4, "Satkhira");
+        bangladesh.put(5, "Cox Bazaar");
+
+
+        for (Map.Entry<Integer, String> cities : bangladesh.entrySet()) {
+            System.out.println(cities.getKey() + " " + cities.getValue());
+        }
+        System.out.println("");
+       
         /*
          * Demonstrate how to use Map that includes storing and retrieving elements.
          * Add List<String> into a Map. Like, Map<String, List<string>> list = new HashMap<String, List<String>>();
          * Use For Each loop and while loop with Iterator to retrieve data.
          *
          * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
-         */
-
+         */    		
+    	
     }
 
 }
