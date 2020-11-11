1c6c1e74776b68aa2a93571b4cd97d8f9606d8f4
njahan98 <51012116+njahan98@users.noreply.github.com>
2020-11-10 19:06:36 -0500

    Update UseQueue.java

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,14 +1,40 @@
 package datastructure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
     public static void main(String[] args) {
         /*
-         * Demonstrate how to use Queue that includes add,peek,remove,pool elements.
+         * Demonstrate how to use Queue that includes add,peek,remove,poll elements.
          * Use For Each loop and while loop with Iterator to retrieve data.
          *
          */
+    	//Add 
+        Queue<String> twilight = new LinkedList<String>();
+        twilight.add("Edward");
+        twilight.add("Bella");
+        twilight.add("Alice");
+        twilight.add("Jasper");
+        twilight.add("Rosalie");
+        twilight.add("Emmett");
+        System.out.println(twilight);
+        
+        //Peek 
+        String head = twilight.peek();
+        System.out.println(twilight);
+        
+        //Remove
+        String oldHead = twilight.poll();
+        String newHead = twilight.peek();
+        System.out.println(oldHead);
+        System.out.println(newHead);
+        
+        for (String Laurett : twilight) {
+        	System.out.println(Laurett);
+        }
 
     }
-
+//need while loop + iterator
 }
