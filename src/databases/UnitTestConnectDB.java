b4adb43138b47106b095b855f4ea98b81ad8455f
njahan98 <51012116+njahan98@users.noreply.github.com>
2020-11-12 00:16:27 -0500

    Update UnitTestConnectDB.java

--- src/databases/UnitTestConnectDB.java
+++ src/databases/UnitTestConnectDB.java
@@ -1,9 +1,22 @@
 package databases;
 
+import org.testng.annotations.Test;
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class UnitTestConnectDB {
-
-    public static void main(String[] args) {
-        ConnectToSqlDB connectToSqlDB = new ConnectToSqlDB();
-
+    @Test
+    public void TestingConnection() {
+        try { //sql is connected and exceptions are created if unable to launch
+            ConnectToDB.connectToSqlDatabase();
+            System.out.println("Sql is launched");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
+
 }
